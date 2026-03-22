c53f4bca82396215a4a6d3a8cd05a6ab6f75162d
Fauzy <fauzymuhamad43@gmail.com>
2026-03-23 05:15:49 +0700

    fix: add cloudinary domain to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   transpilePackages: ["@splinetool/react-spline", "@splinetool/runtime"],
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
-
 export default nextConfig;
